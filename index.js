f3f63887ecc3d185d8da47367d0593a6436b27e9
akgupta0777 <akgupta0777@gmail.com>
2021-10-17 12:06:41 +0530

    Basic site up and running

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("script working")
+const URL = document.getElementById("URL-input");
+const Download = document.getElementById("Download-btn");
+console.log(URL)
+console.log(Download)
+
+Download.addEventListener('click',()=>{
+    console.log(`URL is ${URL.value}`);
+    sendURLToServer(URL.value);
+})
+
+const sendURLToServer = (URL) =>{
+    window.location.href = `https://ytdwnld007.herokuapp.com/download?URL=${URL}`;
+}
