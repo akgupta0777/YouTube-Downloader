4a086589034375ebc37cbc21a2f55aec52cc853a
akgupta0777 <akgupta0777@gmail.com>
2021-10-21 13:28:14 +0530

    Formatted and refactor

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-console.log("script working")
 const URL = document.getElementById("URL-input");
 const Download = document.getElementById("Download-btn");
-console.log(URL)
-console.log(Download)
+const Host = `https://ytytd.herokuapp.com/`
+// const wakeup = fetch(Host)
+//                 .then((res) => res.text())
+//                 .then((data) => console.log(data));
 
-Download.addEventListener('click',()=>{
-    console.log(`URL is ${URL.value}`);
-    sendURLToServer(URL.value);
-})
-
-const sendURLToServer = (URL) =>{
-    window.location.href = `https://ytdwnld007.herokuapp.com/download?URL=${URL}`;
-}
