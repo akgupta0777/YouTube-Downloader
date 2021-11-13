bce20fae7f53f5bbf4b86ec6331c512c66f89228
akgupta0777 <akgupta0777@gmail.com>
2021-11-13 16:46:32 +0530

    Handled errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const URLInput = document.getElementById("URL-input");
 const Download = document.querySelector("button");
-// const videoDiv = document.getElementById('videos')
-const Host = `https://ytytd.herokuapp.com`
+const Host = `https://ytytd.herokuapp.com`;
 const formatInfo = {
     'mp4':'video',
     '3gp':'video',
@@ -12,73 +11,83 @@ const formatInfo = {
     'ogg':'audio',
     'aac':'audio',
     'm4a':'audio'
-}
+};
 const wakeup = fetch(Host)
                 .then((res) => res.text())
-                .then((data) => data);
+                .then((data) => console.log('services started'));
 
 const loadVideo = async () => {
-    document.querySelector('div#videos').style.display = 'none'
-    document.querySelector('div.animation').style.display = 'block';
-    const url = URLInput.value;
-    const videoInfo = await fetch(`${Host}/getVideo?url=${url}`)
-                    .then((res) => res.json());
-    const {thumbnail,title,channel,length} = videoInfo[0];
-    document.querySelector('img#thumbnail').src = thumbnail;
-    document.querySelector('p#title').innerText = title;
-    document.querySelector('p#channel').innerText = channel;
-    document.querySelector('p#duration').innerText = `Duration: ${length}`;
-    const [videoTable,audioTable] = document.querySelectorAll('table');
-    let videoHTML = `
-    <tbody>
-    <tr>
-      <th>Resolution</th>
-      <th>Size</th>
-      <th>Download</th>
-    </tr>`;
-    let audioHTML = `
-    <tbody>
-    <tr>
-      <th>Resolution</th>
-      <th>Size</th>
-      <th>Download</th>
-    </tr>`;
-    const videos = videoInfo[1];
-    videos.forEach(media => {
-        let {value,format,size} = media;
-        const link = `${Host}/download?url=${url}&v=${value}&f=${format}`;
-        if(!!formatInfo[media.format]){
-        let HTML = `
-        <tr>
-            <td class="align-middle py-4">
-                <span class="d-block"> ${value} ${format}</span>
-            </td>
-            <td class="align-middle">
-                <span class="d-block"> ${size} </span>
-            </td>
-            <td class="align-middle">
-                <a
-                class="btn btn-primary btn-sm mb-0"
-                href=${link}
-                title="Download"
-                >
-                Download
-                </a>
-            </td>
-        </tr>`
-        if(formatInfo[media.format] == 'video'){
-            videoHTML+= HTML;
-        }else{
-            audioHTML+= HTML;
+    try{
+        document.querySelector('div#error').style.display = 'none';
+        document.querySelector('div#videos').style.display = 'none';
+        document.querySelector('div.animation').style.display = 'block';
+        const url = URLInput.value;
+        const videoInfo = await fetch(`${Host}/getVideo?url=${url}`)
+                        .then((res) => res.json());
+        if(videoInfo.error){
+            document.querySelector('div.animation').style.display = 'none';
+            document.querySelector('div#error').style.display = 'block';
+            return ;
         }
-    }else return ;
-    });
-    videoHTML+='</tbody>';
-    audioHTML+='</tbody>';
-    videoTable.innerHTML = videoHTML;
-    audioTable.innerHTML = audioHTML;
-    document.querySelector('div.animation').style.display = 'none';
-    document.querySelector('div#videos').style.display = 'block'
+        const {thumbnail,title,channel,length} = videoInfo[0];
+        document.querySelector('img#thumbnail').src = thumbnail;
+        document.querySelector('p#title').innerText = title;
+        document.querySelector('p#channel').innerText = channel;
+        document.querySelector('p#duration').innerText = `Duration: ${length}`;
+        const [videoTable,audioTable] = document.querySelectorAll('table');
+        let videoHTML = `
+        <tbody>
+        <tr>
+            <th>Resolution</th>
+            <th>Size</th>
+            <th>Download</th>
+        </tr>`;
+        let audioHTML = `
+        <tbody>
+            <tr>
+                <th>Resolution</th>
+                <th>Size</th>
+                <th>Download</th>
+            </tr>`;
+        const videos = videoInfo[1];
+        videos.forEach(media => {
+            let {value,format,size} = media;
+            const link = `${Host}/download?url=${url}&v=${value}&f=${format}`;
+            if(!!formatInfo[media.format]){
+                let HTML = `
+                <tr>
+                    <td class="align-middle py-4">
+                        <span class="d-block"> ${value} ${format}</span>
+                    </td>
+                    <td class="align-middle">
+                        <span class="d-block"> ${size} </span>
+                    </td>
+                    <td class="align-middle">
+                        <a
+                        class="btn btn-primary btn-sm mb-0"
+                        href=${link}
+                        title="Download"
+                        >
+                        Download
+                        </a>
+                    </td>
+                </tr>`;
+                if(formatInfo[media.format] == 'video'){
+                    videoHTML+= HTML;
+                }else{
+                    audioHTML+= HTML;
+                }
+            }else return ;
+        });
+        videoHTML+='</tbody>';
+        audioHTML+='</tbody>';
+        videoTable.innerHTML = videoHTML;
+        audioTable.innerHTML = audioHTML;
+        document.querySelector('div.animation').style.display = 'none';
+        document.querySelector('div#videos').style.display = 'block'
+    }catch{
+
+    }
 }
 
 Download.addEventListener('click',loadVideo);
