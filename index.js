03d5822a50ca337c851f7a15b779674f2ae3266e
akgupta0777 <akgupta0777@gmail.com>
2022-10-19 11:11:28 +0530

    Added alerts for errors and refactors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const URLInput = document.getElementById("URL-input");
 const download = document.querySelector("button");
 const indicator = document.querySelector("#indicator");
-const statusText = document.querySelector("#status-text")
+const statusText = document.querySelector("#status-text");
+const error = document.querySelector('div#error');
+const videos = document.querySelector('div#videos');
+const animation = document.querySelector('div.animation');
 const HOST = `https://ytytd.herokuapp.com`;
+let Exhausted = false;
 const formatInfo = {
     'mp4':'video',
     '3gp':'video',
@@ -14,6 +18,7 @@ const formatInfo = {
     'aac':'audio',
     'm4a':'audio'
 };
+
 const wakeup = fetch(HOST)
                 .then((res) => res.text())
                 .then((data) => {
@@ -25,23 +30,29 @@ const wakeup = fetch(HOST)
                         indicator.classList.add("available");
                         statusText.textContent="API Status : Available "
                     }
+                    Exhausted = false;
+                    document.querySelector("#exhausted").style.display = 'none';
                 }).catch((err)=>{
                     indicator.classList.remove("looking");
                     indicator.classList.add("down");
                     statusText.textContent="API Status : Down "
+                    console.log("err",err)
+                    Exhausted = true;
+                    document.querySelector("#exhausted").style.display = 'block';
                 }); 
 
 const loadVideo = async () => {
+    if(Exhausted) return;
     try{
-        document.querySelector('div#error').style.display = 'none';
-        document.querySelector('div#videos').style.display = 'none';
-        document.querySelector('div.animation').style.display = 'block';
+        error.style.display = 'none';
+        videos.style.display = 'none';
+        animation.style.display = 'block';
         const url = URLInput.value;
         const videoInfo = await fetch(`${HOST}/getVideo?url=${url}`)
                         .then((res) => res.json());
         if(videoInfo.error){
-            document.querySelector('div.animation').style.display = 'none';
-            document.querySelector('div#error').style.display = 'block';
+            animation.style.display = 'none';
+            error.style.display = 'block';
             return ;
         }
         const {thumbnail,title,channel,length} = videoInfo[0];
@@ -98,11 +109,11 @@ const loadVideo = async () => {
         audioHTML+='</tbody>';
         videoTable.innerHTML = videoHTML;
         audioTable.innerHTML = audioHTML;
-        document.querySelector('div.animation').style.display = 'none';
-        document.querySelector('div#videos').style.display = 'block'
+        animation.style.display = 'none';
+        videos.style.display = 'block'
     }catch{
-        document.querySelector('div.animation').style.display = 'none';
-        document.querySelector('div#error').style.display = 'block';
+        animation.style.display = 'none';
+        error.style.display = 'block';
         return ;
     }
 }
